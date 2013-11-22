e4162be3838ae0f021055713351bc44bd148ddf2
AravinthChandran <arvinthchandran@gmail.com>
2013-11-22 23:22:32 +0530

    Project under source control

---
+++ Matchismo/Matchismo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Matchismo
+//
+//  Created by AravinthChandran on 22/11/13.
+//  Copyright (c) 2013 AravinthChandran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CardGameAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CardGameAppDelegate class]));
+    }
+}
