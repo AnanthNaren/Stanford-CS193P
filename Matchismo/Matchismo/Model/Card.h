ce8de762dccdad07f33e863794fa029024b5f439
AravinthChandran <arvinthchandran@gmail.com>
2013-11-22 23:41:15 +0530

    Added a Card Model

---
+++ Matchismo/Matchismo/Model/Card.h
@@ -0,0 +1,13 @@
+//
+//  Card.h
+//  Matchismo
+//
+//  Created by AravinthChandran on 22/11/13.
+//  Copyright (c) 2013 AravinthChandran. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Card : NSObject
+
+@end
---
+++ Matchismo/Matchismo/Model/Card.m
@@ -0,0 +1,13 @@
+//
+//  Card.m
+//  Matchismo
+//
+//  Created by AravinthChandran on 22/11/13.
+//  Copyright (c) 2013 AravinthChandran. All rights reserved.
+//
+
+#import "Card.h"
+
+@implementation Card
+
+@end
