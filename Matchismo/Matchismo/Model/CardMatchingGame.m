85050e930666a3079c3e8295760b0f0e7b684411
AravinthChandran <arvinthchandran@gmail.com>
2013-11-23 13:53:46 +0530

    Card Matching Game logic Implemented

--- Matchismo/Matchismo/Model/CardMatchingGame.m
+++ Matchismo/Matchismo/Model/CardMatchingGame.m
@@ -26,17 +26,73 @@
 
 -(instancetype)initWithCardCount:(NSUInteger)count usingDeck:(Deck *)deck{
     self  = [super init]; //Super's Designated Initializer (of NSObject)
-    
     if(self){
-        
         for(int i=0; i<count; i++){
             Card *card = [deck drawRandomCard];
-            [self.cards addObject:card];
+            
+            //protecting against nill.
+            if(card){
+                [self.cards addObject:card];
+            }else{
+                self = nil;
+                break;
+            }
         }
-        
     }
-    
     return self;
 }
+
+
+-(Card *)cardAtIndex:(NSUInteger)index{
+    
+    return (index < [self.cards count]) ? self.cards[index] : nil;
+}
+
+
+static const int MISMATCH_PENALTY = 2;
+static const int MISMATCH_BONUS = 2;
+static const int COST_TO_CHOOSE = 1;
+
+- (void)chooseCardAtIndex:(NSUInteger)index{
+    Card *card = [self cardAtIndex:index];
+    
+    if(!card.isMatched){
+        
+        //Toggle the card state.
+        if(card.isChosen){
+            card.chosen = NO;
+        }else{
+            //Match against other chosen cards
+            for(Card *otherCard in self.cards){
+                if(otherCard.isChosen && !otherCard.isMatched){
+                    int matchScore = [card match:@[otherCard]];
+                    if(matchScore){
+                        self.score += matchScore * MISMATCH_BONUS;
+                        card.matched = YES;
+                        otherCard.matched = YES;
+                    }else{
+                        self.score -= MISMATCH_PENALTY;
+                        otherCard.chosen = NO;
+                    }
+                    break;  //For choosing only 2 cards
+                }
+                
+            }
+            self.score += COST_TO_CHOOSE;
+        card.chosen = YES;
+            
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
 
 @end
