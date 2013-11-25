69559016cd2ecd59a9933d323bb9396b03e6fd61
AravinthChandran <arvinthchandran@gmail.com>
2013-11-25 11:16:57 +0530

    Playing card basic implementation is done

--- Matchismo/Matchismo/CardGameViewController.m
+++ Matchismo/Matchismo/CardGameViewController.m
@@ -11,58 +11,23 @@
 #import "PlayingCardDeck.h"
 
 @interface CardGameViewController ()
-@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cardButtons;
 @property (strong, nonatomic) CardMatchingGame *game;
 @property (strong, nonatomic) Deck *deck;
-@property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
-@end 
+@end
 
 @implementation CardGameViewController
 
--(CardMatchingGame *)game{
-    if(!_game)_game = [[CardMatchingGame alloc] initWithCardCount:[self.cardButtons count]
-                                                        usingDeck:[self createDeck]];
-return _game;
-                       
-}
-                       
--(Deck *)deck{
-    if(!_deck)_deck = [self createDeck];
-    return _deck;
-}
-
--(Deck*) createDeck{
-    return [[PlayingCardDeck alloc]init];
-}
-
-
 - (IBAction)touchCardButton:(UIButton *)sender {
-    int chooseButtonIndex = [self.cardButtons indexOfObject:sender];
-    [self.game chooseCardAtIndex:chooseButtonIndex];
-    [self updateUI];
-}
-
-
--(void) updateUI{
-    for(UIButton *cardButton in self.cardButtons){
-        int cardButtonIndex = [self.cardButtons indexOfObject:cardButton];
-        Card *card = [self.game cardAtIndex:cardButtonIndex];
-        [cardButton setTitle:[self titleForCard:card] forState:UIControlStateNormal];
-        [cardButton setBackgroundImage:[self backgroundForCard:card] forState:UIControlStateNormal];
-        cardButton.enabled = !card.isMatched;
-        self.scoreLabel.text = [NSString stringWithFormat:@"score: %d",self.game.score];
+    if([sender.currentTitle length]){
+        [sender setBackgroundImage:[UIImage imageNamed:@"cardback"]
+                          forState:UIControlStateNormal];
+        [sender setTitle:@"" forState:UIControlStateNormal];
+    }else{
+        [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"]
+                          forState:UIControlStateNormal];
+        [sender setTitle:@"A♣︎" forState:UIControlStateNormal];
     }
 }
 
-
--(NSString *)titleForCard:(Card *)card{
-    return card.isChosen ? card.contents : @"";
-}
-
-
--(UIImage *)backgroundForCard:(Card *)card{
-    
-    return [UIImage imageNamed:card.isChosen ? @"cardfront" : @"cardback"];
-}
 
 @end
