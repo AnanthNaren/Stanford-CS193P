e4a4561bf211d93c707d9849d17ef55e15402af5
AravinthChandran <arvinthchandran@gmail.com>
2013-11-25 14:47:11 +0530

    Competed Assignment-1

--- Matchismo/Matchismo/CardGameViewController.m
+++ Matchismo/Matchismo/CardGameViewController.m
@@ -7,23 +7,46 @@
 //
 
 #import "CardGameViewController.h"
+#import "PlayingCardDeck.h"
 
 @interface CardGameViewController ()
-
+@property (weak, nonatomic) IBOutlet UITextField *flipsLabel;
+@property (nonatomic) int flipsCount;
+@property (nonatomic, strong) Deck *deck;
 @end 
 
 @implementation CardGameViewController
 
+
+-(void)setFlipsCount:(int)flipsCount{
+    _flipsCount = flipsCount;
+    self.flipsLabel.text = [NSString stringWithFormat:@"Flips : %d", _flipsCount];
+}
+
+-(Deck *)deck{
+    if(!_deck) _deck = [[PlayingCardDeck alloc]init];
+    return _deck;
+}
+
 - (IBAction)touchCardButton:(UIButton*)sender {
     
     if([sender.currentTitle length ]){
-        [sender setBackgroundImage:[UIImage imageNamed:@"cardback"]
-                          forState:UIControlStateNormal];
-        [sender setTitle:@"" forState:UIControlStateNormal];
-    }else{
-        [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"]
-                          forState:UIControlStateNormal];
-        [sender setTitle:@"A♣︎" forState:UIControlStateNormal];
+            [sender setBackgroundImage:[UIImage imageNamed:@"cardback"]
+                              forState:UIControlStateNormal];
+            [sender setTitle:@"" forState:UIControlStateNormal];
+            
+        }else{
+            Card *card = [self.deck drawRandomCard];
+        if(card){
+            [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"]
+                              forState:UIControlStateNormal];
+            [sender setTitle:[card contents]
+                    forState:UIControlStateNormal];
+            self.flipsCount++;
+        }else{
+            self.flipsLabel.text = [NSString stringWithFormat:@"There are no card to flip !!!"];
+
+        }
     }
 }
 
