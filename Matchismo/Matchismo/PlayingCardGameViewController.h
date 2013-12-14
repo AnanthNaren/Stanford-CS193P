82ae5046a5bfe47785feaff0adf39be65f35320a
AravinthChandran <arvinthchandran@gmail.com>
2013-12-14 13:19:45 +0530

    creating a new playingCardGameViewController

---
+++ Matchismo/Matchismo/PlayingCardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardGameViewController.h
+//  Matchismo
+//
+//  Created by AravinthChandran on 14/12/13.
+//  Copyright (c) 2013 AravinthChandran. All rights reserved.
+//
+
+#import "CardGameViewController.h"
+
+@interface PlayingCardGameViewController : CardGameViewController
+
+@end
