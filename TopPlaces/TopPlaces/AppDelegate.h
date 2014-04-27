3b17f42952355da98be5c9ae3dabeabd08fe77cc
AravinthChandran <arvinthchandran@gmail.com>
2014-04-28 01:23:03 +0530

    Starting TopPlaces App

---
+++ TopPlaces/TopPlaces/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TopPlaces
+//
+//  Created by Aravinth Chandran on 28/04/14.
+//  Copyright (c) 2014 Arvch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TopPlaces/TopPlaces/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TopPlaces
+//
+//  Created by Aravinth Chandran on 28/04/14.
+//  Copyright (c) 2014 Arvch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
